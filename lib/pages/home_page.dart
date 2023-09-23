c87f2ecfaa35856d59c6cddd19ee9e60cdaf2d6f
bbalajo2 <56073751+bbalajo2@users.noreply.github.com>
2023-09-23 14:19:58 +0100

    trying to making the id autoincrement

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:js';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/note_data.dart';  // Import NoteData
+import '../models/note.dart';
+import '../models/note_data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,11 +13,21 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+class _HomePageState extends State<HomePage> {
+
 void addNewNote() {
+  //create a new id for the note
+  int id = Provider.of<NoteData>(context, listen: false).getAllNotes().length;
+  Note newNote = Note(
+    id: id,
+    text: "",
+  );
+  
+  //edit the note
   
 }
 
-class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Consumer<NoteData>(
